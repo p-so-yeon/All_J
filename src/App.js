03a7acabe30e6a84e37108a36cd00c0f5194ada9
may <soyeon1056@naver.com>
2023-12-12 10:31:13 +0900

    ga 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
 import Landing from "./Landing";
-
+import ReactGA from "react-ga4";
 import Ready from "./Ready";
+import React, { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import React from "react";
+
+const gaTrackingId = process.env.REACT_APP_GOOGLE_ANALYTICS;
+ReactGA.initialize(gaTrackingId, { debug: true });
 function App() {
+  useEffect(() => {
+    ReactGA.set({ page: window.location.pathname });
+    ReactGA.event({
+      category: "Navigation",
+      action: "PageView",
+      label: window.location.pathname,
+    });
+  }, []);
   return (
     <div className="App">
       <BrowserRouter>
--- src/Ready.js
+++ src/Ready.js
@@ -3,6 +3,7 @@ import "./Ready.css";
 const Ready = () => {
   return (
     <div className="Ready">
+      <head></head>
       <div>아직 서비스 준비중입니다 </div>
       <div> 완성 후 다시 찾아 올게요</div>
       <div> 😭 </div>
